990ea63b365ffa03594c852ced2b26616ecd7b12
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-08 14:21:47 +0100

    modified js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function Button(text){
+function Button(text) {
     this.text = text;
 }
 
 Button.prototype = {
-    create: function() {
+    create: function () {
         var self = this;
         this.element = document.createElement('button');
         this.element.innerText = this.text;
-        this.element.addEventListener('click', function() {
-            alert(self.text);
         document.body.appendChild(this.element);
+        this.element.addEventListener('click', function () {
+            alert(self.text);
         });
     }
 }
@@ -18,3 +18,6 @@ var btn1 = new Button('Hello');
 
 btn1.create();
 
+var btn2 = new Button('Ciao');
+
+btn2.create();
